c5cc42446d34f3b595a78672e008d141c4e5000d
jonasgroenbek <jonasgroenbek@gmail.com>
2022-12-03 16:45:07 +0100

    removed logs

--- target/debug/deps/file_shortener-599bfafdf60c38e5.d
+++
@@ -1,5 +0,0 @@
-/home/jonas/Projects/file-shortener/target/debug/deps/file_shortener-599bfafdf60c38e5.rmeta: src/main.rs
-
-/home/jonas/Projects/file-shortener/target/debug/deps/file_shortener-599bfafdf60c38e5.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/file_shortener-5f7c7a4988c19026.d
+++
@@ -1,5 +0,0 @@
-/home/jonas/Projects/file-shortener/target/debug/deps/file_shortener-5f7c7a4988c19026.rmeta: src/main.rs
-
-/home/jonas/Projects/file-shortener/target/debug/deps/file_shortener-5f7c7a4988c19026.d: src/main.rs
-
-src/main.rs:
